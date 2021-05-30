2135a9d6432acc8b9a9a817bef4e96bdefa25e03
Jon K <Johnakao@gmail.com>
2021-05-30 10:13:43 -0700

    Recursive Solution for Letter Combinations/Tests

--- medianSortedArrays.js
+++ medianSortedArrays.js
@@ -1,9 +1,7 @@
-// O(N^2)
-
+// O(N)
 function findMedianSortedArrays(num1, num2) {
   let combinedSortedArr = [...num1, ...num2].sort((a,b) => a - b)
   let medianIdx = Math.floor(combinedSortedArr.length/2)
-  console.log(combinedSortedArr, medianIdx)
   return (combinedSortedArr.length % 2 === 1) ? 
   combinedSortedArr[medianIdx] : 
   (combinedSortedArr[medianIdx] + combinedSortedArr[medianIdx - 1]) / 2

49c858dcd1378b2826e5dcb82d3e2ede896e74b4
Jon K <Johnakao@gmail.com>
2021-06-05 08:53:40 -0700

    Fixed bug in max units tests

--- maximumUnitsOnTruck.test.js
+++ maximumUnitsOnTruck.test.js
@@ -4,5 +4,5 @@ test('calculates maximum units on a truck', () => {
   expect(maximumUnits([[1,3], [2,2], [3,1]], 4)).ToBe(8)
 })
 test('calculates maximum units on a truck', () => {
-  expect(maximumUnits([[5,10],[2,5],[4,7],[3,9]], 10).ToBe(91)
+  expect(maximumUnits([[5,10],[2,5],[4,7],[3,9]], 10)).ToBe(91)
 })

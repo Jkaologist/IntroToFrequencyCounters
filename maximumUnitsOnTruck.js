dc6b206525c56d1a1a6b6e060859409ee0fe5a06
Jon K <Johnakao@gmail.com>
2021-06-07 12:44:10 -0700

    O(nlogn) soln to maxUnits prob

--- maximumUnitsOnTruck.js
+++ maximumUnitsOnTruck.js
@@ -9,45 +9,42 @@ function maximumUnits(boxTypes, truckSize) {
    * placed on the truck.
    */
 
-  // Brute force approach for finding max units O(n^2) TC O(1) SC
-  let totalUnits = 0
-  let remainingTruckSpace = truckSize
+//   // Brute force approach for finding max units O(n^2) TC O(1) SC
+//   let totalUnits = 0
+//   let remainingTruckSpace = truckSize
 
-  while (remainingTruckSpace > 0) {
-    let maxUnitBoxIdx = findMaxUnitBox(boxTypes)
-    if (maxUnitBoxIdx === -1) break
-    let boxCount = Math.min(remainingTruckSpace, boxTypes[maxUnitBoxIdx][0])
-    totalUnits += boxCount * boxTypes[maxUnitBoxIdx][1]
-    remainingTruckSpace -= boxCount
-    boxTypes[maxUnitBoxIdx][1] = -1
+//   while (remainingTruckSpace > 0) {
+//     let maxUnitBoxIdx = findMaxUnitBox(boxTypes)
+//     if (maxUnitBoxIdx === -1) break
+//     let boxCount = Math.min(remainingTruckSpace, boxTypes[maxUnitBoxIdx][0])
+//     totalUnits += boxCount * boxTypes[maxUnitBoxIdx][1]
+//     remainingTruckSpace -= boxCount
+//     boxTypes[maxUnitBoxIdx][1] = -1
+//   }
+//   return totalUnits
+// }
+
+// function findMaxUnitBox(boxTypes) {
+//   let maxUnitBoxIdx = -1
+//   let maxUnits = 0
+//   for (let i = 0; i < boxTypes.length; i++) {
+//     if (boxTypes[i][1] > maxUnits) {
+//       maxUnits = boxTypes[i][1]
+//       maxUnitBoxIdx = i
+//     }
+//   }
+//   return maxUnitBoxIdx
+// }
+
+// // sort by highest method O(nlogn) TC O(n) SC
+  boxTypes.sort((a, b) => b[1] - a[1])
+  let totalUnits = 0
+  for (let box of boxTypes) {
+    boxCount = Math.min(box[0], truckSize)
+    totalUnits += boxCount * box[1]
+    truckSize -= boxCount
+    if (truckSize === 0) break
   }
   return totalUnits
 }
-
-function findMaxUnitBox(boxTypes) {
-  let maxUnitBoxIdx = -1
-  let maxUnits = 0
-  for (let i = 0; i < boxTypes.length; i++) {
-    if (boxTypes[i][1] > maxUnits) {
-      maxUnits = boxTypes[i][1]
-      maxUnitBoxIdx = i
-    }
-  }
-  return maxUnitBoxIdx
-}
-
-// // sort by highest method O(nlogn) TC O(n) SC
-// return boxTypes
-//   .sort((a, b) => b[1] - a[1])
-//   .reduce(
-//     function(acc, val) {
-//       if (truckSize > val[1]) { 
-//         truckSize -= val[1]
-//         return acc + val[1] * val[0]
-//       }
-//       else {
-//         return acc + val[0] * truckSize 
-//       }      
-//     }, 0)
-// }
 module.exports = maximumUnits

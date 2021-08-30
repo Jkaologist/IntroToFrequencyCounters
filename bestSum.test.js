c3fceb9666f7f1cecb744d9ff8b171f1196e3d98
Jon K <Johnakao@gmail.com>
2021-08-30 15:28:47 -0700

    Tested Optomization Algo

--- bestSum.test.js
+++ bestSum.test.js
@@ -1,12 +1,14 @@
 const bestSum = require('./bestSum')
 
 test('can sum the elements of the array to reach target', () => {
-  expect(bestSum(8, [2, 3, 5])).toStrictEqual([2, 2, 2, 2])});
+  expect(bestSum(8, [2, 3, 5])).toStrictEqual([5, 3])});
 test('can sum the elements of the array to reach target', () => {
-  expect(bestSum(7, [5, 3, 4, 7])).toStrictEqual([4, 3])});
+  expect(bestSum(7, [5, 3, 4, 7])).toStrictEqual([7])});
 test('can sum the elements of the array to reach target', () => {
   expect(bestSum(7, [2, 4])).toBe(null)});
 test('can sum the elements of the array to reach target', () => {
   expect(bestSum(0, [1, 2, 3])).toStrictEqual([])});
 test('can sum the elements of the array to reach target', () => {
   expect(bestSum(300, [7, 14])).toBe(null)});
+test('can sum the elements of the array to reach target', () => {
+  expect(bestSum(100, [1, 2, 5, 25])).toStrictEqual([25, 25, 25, 25])});

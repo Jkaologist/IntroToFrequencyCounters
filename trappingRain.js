faa44e138af776ae9862bd990f632aaa6dc4b5ae
Jon K <Johnakao@gmail.com>
2022-01-07 10:17:55 -0800

    trapping rain two pointer javascript

--- trappingRain.js
+++ trappingRain.js
@@ -1,17 +1,39 @@
+// const trap = (height) => {
+//   let waterTrapped = 0;
+//   for (let i = 0; i < height.length; i++) {
+//     let leftMax = 0,
+//       rightMax = 0;
+//     for (let j = i; j >= 0; j--) {
+//       leftMax = Math.max(leftMax, height[j]);
+//     }
+//     for (let k = i; k < height.length; k++) {
+//       rightMax = Math.max(rightMax, height[k]);
+//     }
+//     waterTrapped += Math.min(leftMax, rightMax) - height[i];
+//   }
+//   return waterTrapped;
+// };
+
 const trap = (height) => {
-  let waterTrapped = 0;
-  for (let i = 0; i < height.length; i++) {
-    let leftMax = 0,
-      rightMax = 0;
-    for (let j = i; j >= 0; j--) {
-      leftMax = Math.max(leftMax, height[j]);
+  let left = 0,
+    right = height.length - 1,
+    leftMax = 0,
+    rightMax = 0,
+    ans = 0;
+  while (left < right) {
+    if (height[left] < height[right]) {
+      leftMax <= height[left]
+        ? (leftMax = height[left])
+        : (ans += leftMax - height[left]);
+      ++left;
+    } else {
+      rightMax <= height[right]
+        ? (rightMax = height[right])
+        : (ans += rightMax - height[right]);
+      --right;
     }
-    for (let k = i; k < height.length; k++) {
-      rightMax = Math.max(rightMax, height[k]);
-    }
-    waterTrapped += Math.min(leftMax, rightMax) - height[i];
   }
-  return waterTrapped;
+  return ans;
 };
 
 console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])); //6
